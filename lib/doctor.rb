0c3c0b401401dbac3427b772dfab2a6448de70d0
vibraniumforge <gregm9999@gmail.com>
2019-09-18 13:13:55 -0700

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,33 @@
+require "pry"
+class Doctor
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def appointments
+    Appointment.all.select do |app|
+      app.doctor == self
+    end
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+
+  def patients
+    appointments.map do |app|
+      app.patient
+    end
+  end
+
+end
